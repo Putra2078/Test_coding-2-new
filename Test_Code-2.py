a6a7fc5507e70f42958bebc6c5a51bb10829bfed
Putra2078 <141045021+Putra2078@users.noreply.github.com>
2023-07-31 21:25:58 +0700

    Add files via upload

---
+++ Test_Code-2.py
@@ -0,0 +1,10 @@
+angka1 = input('Tulis angka pertama:1')
+angka2 = input('Tulis angka kedua:2')
+
+sum = int(angka1) + int(angka2)
+
+print('hasil ppenjumlahan{0} dan {1} adalah {2}'.format(angka1, angka2, sum))
+
+Tulis angka pertama: 1
+Tulis angka kedua: 2
+Hasil Penjumlahan 1 dan 2 adalah 3
